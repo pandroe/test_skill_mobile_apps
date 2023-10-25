4b4d56b83616bbae78e1a6ecd0e83980bb4d0f18
pandroe <daffapandu2304@gmail.com>
2023-10-25 08:05:38 +0700

    feature login google

---
+++ lib/controllers/user_controllers.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class UserLoginController {
+  static Future<User?> loginWithGoogle() async {
+    final googleAccount =
+        await GoogleSignIn().signIn(); // Tambahkan 'await' di sini
+    final googleAuth =
+        await googleAccount?.authentication; // Tambahkan 'await' di sini
+
+    final credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth?.accessToken,
+      idToken: googleAuth?.idToken,
+    );
+
+    final userCredential =
+        await FirebaseAuth.instance.signInWithCredential(credential);
+    return userCredential.user;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'providers/list_users_provider.dart';
-import 'views/image_picker_screen/views/image_picker_screen.dart';
+import 'utils/firebase_options.dart';
+import 'views/login_screen/views/login_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     ChangeNotifierProvider(
       create: (context) => UserProvider(),
@@ -18,7 +22,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ImagePickerScreen(),
+      home: LoginScreen(),
     );
   }
 }
